4c6483278aa36cdc1c94a8173f977555a44e1523
Weronika Rymarz <weronika92rymarz@gmail.com>
2019-07-28 00:03:11 +0200

    new project and dependencies in pom file

---
+++ src/main/java/com/werrmz/zalando_selenium/App.java
@@ -0,0 +1,13 @@
+package com.werrmz.zalando_selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
